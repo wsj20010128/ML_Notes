3bfc3a02da733240d11ca3b096115accec6ff72d
WitnessJ <1270890350@qq.com>
2022-01-08 09:36:36 +0800

    update metrics

---
+++ Linear-Regression/02-Regression-Metrics-MSE-vs-MAE/playML/metrics.py
@@ -0,0 +1,46 @@
+import numpy as np
+from math import sqrt
+
+
+def accuracy_score(y_true, y_predict):
+    assert y_true.shape[0] == y_predict.shape[0], \
+        "the size of y_true must be equal to the size of y_predict"
+
+    return sum(y_true == y_predict) / len(y_true)
+
+
+def mean_squared_error(y_true, y_predict):
+    """
+    calculate MSE between y_true and y_predict
+    :param y_true:
+    :param y_predict:
+    :return:
+    """
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
+
+    return np.sum((y_true - y_predict)**2) / len(y_true)
+
+
+def root_mean_squared_error(y_true, y_predict):
+    """
+    calculate RMSE between y_true and y_predict
+    :param y_true:
+    :param y_predict:
+    :return:
+    """
+
+    return sqrt(mean_squared_error(y_true, y_predict))
+
+
+def mean_absolute_error(y_true, y_predict):
+    """
+    calculate MAEbetween y_true and y_predict
+    :param y_true:
+    :param y_predict:
+    :return:
+    """
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
+
+    return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
